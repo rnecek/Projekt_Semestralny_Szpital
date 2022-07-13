78cd94b73ad0e9d8422aa495c23b73ccde53fb8d
rnecek <80617903+rnecek@users.noreply.github.com>
2022-07-13 10:28:34 +0200

    Add project files.

---
+++ ProjektSemestralny/WPF_1_ZS.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektSemestralny
+{
+    /// <summary>
+    /// Interaction logic for WPF_1_ZS.xaml
+    /// </summary>
+    public partial class WPF_1_ZS : Window
+    {
+        public WPF_1_ZS()
+        {
+            InitializeComponent();
+        }
+    }
+}
